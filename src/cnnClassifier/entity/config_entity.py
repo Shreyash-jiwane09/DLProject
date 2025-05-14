8fc22bd6fed84235b8c38f1784b0517f44dc400e
Shreyash-jiwane09 <jiwane.shrey09@gmail.com>
2025-05-14 21:10:09 +0530

    add data ingestion, model preparation, training, and evaluation config classes

---
+++ src/cnnClassifier/entity/config_entity.py
@@ -0,0 +1,48 @@
+
+from dataclasses import dataclass
+from pathlib import Path
+
+
+@dataclass(frozen=True)
+class DataIngestionConfig:
+    root_dir: Path
+    source_URL: str
+    local_data_file: Path
+    unzip_dir: Path
+
+
+@dataclass(frozen=True)
+class PrepareBaseModelConfig:
+    root_dir: Path
+    base_model_path: Path
+    updated_base_model_path: Path
+    params_image_size: list
+    params_learning_rate: float
+    params_include_top: bool
+    params_weights: str
+    params_classes: int
+
+
+
+@dataclass(frozen=True)
+class TrainingConfig:
+    updated_base_model_path: Path
+    trained_model_path: Path
+    train_data: Path             
+    val_data: Path               
+    test_data: Path              
+    params_epochs: int
+    params_batch_size: int
+    params_image_size: tuple
+    params_is_augmentation: bool
+
+
+
+@dataclass(frozen=True)
+class EvaluationConfig:
+    path_of_model: Path
+    training_data: Path
+    all_params: dict
+    #mlflow_uri: str
+    params_image_size: list
+    params_batch_size: int
